b4fac368ac316fefbecdaa884f5ad574582889fc
CoderAWei <85542563+CoderAWei@users.noreply.github.com>
2023-04-17 21:33:53 +0800

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,22 @@
+# RPAS FE Template
+
+## 1. Run app
+
+- ``npm i``
+- ``npm start``
+
+## 2. ESlint config
+
+### 2.1 add VS Code setting
+
+> - Open vscode setting page and open ``setting.json`` file.
+> - add below configs
+
+```json
+"editor.indentSize": "tabSize",
+"editor.formatOnSave": true,
+"editor.tabSize": 4,
+"editor.codeActionsOnSave": {
+    "source.fixAll.eslint": true,
+},
+```
---
+++ craco.config.js
@@ -0,0 +1,34 @@
+const CracoLessPlugin = require('craco-less')
+const path = require("path")
+
+module.exports = {
+	webpack: {
+		alias: {
+			"@": path.resolve(__dirname, "src")
+		}
+	},
+	publicPath: './',
+	plugins: [
+		{
+			plugin: CracoLessPlugin,
+			options: {
+				lessLoaderOptions: {
+					lessOptions: {
+						modifyVars: {
+							'@primary-color': '#D31145',
+							'@border-radius-base': '4px',
+							'@font-size-base': '16px',
+							'@text-color': '#333D47', // 主文本色
+							'@heading-color': '#333333', // 标题色
+							'@success-color': '#3DA758', // 成功色
+							'@warning-color': '#FFC107', // 警告色
+							'@error-color': '#DC3545', // 警告色
+							hack: `true; @import (reference) "${path.resolve(__dirname, './src/styles/variables.less')}";` // 将variables.ess里的变量变成全局
+						},
+						javascriptEnabled: true
+					}
+				}
+			}
+		}
+	]
+}
