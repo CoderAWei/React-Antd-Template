7d4344e42a2cb867b316b71e96a0315a12268c89
CoderAWei <xzwei0914@gmail.com>
2023-05-07 10:33:05 +0800

    Add husky

---
+++ .lintstagedrc.js
@@ -0,0 +1,5 @@
+// Lint-staged仅仅是文件过滤器，不会帮你格式化任何东西
+module.exports = {
+    "src/**/\*{js,jsx,ts,tsx,md,html}": ["eslint", "prettier --write"],
+    "src/**/*.{css,less}": ["stylelint --fix", "git add"],
+}
