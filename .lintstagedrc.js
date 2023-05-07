a9ec8ef4dfd14d4af24ab80e61e8d53a90a41430
CoderAWei <xzwei0914@gmail.com>
2023-05-07 22:03:08 +0800

    🚀

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 // Lint-staged仅仅是文件过滤器，不会帮你格式化任何东西
 module.exports = {
-    "src/**/\*{js,jsx,ts,tsx,md,html}": ["eslint"],
+    "src/**/\*{js,jsx,ts,tsx,md,html}": ["eslint --fix", "git add"],
     "src/**/*.{css,less}": ["stylelint --fix", "git add"],
 }
